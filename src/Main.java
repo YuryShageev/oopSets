11f1cb0ef4ea06db916d27eab4a62c21aa409c5d
YuryShageev <schageev.yur@yandex.ru>
2022-12-12 14:47:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Домашка 1
+
+        Set<Product> basket = new HashSet<>();
+
+        Product apples = new Product("Яблоки", 0, 160f);
+        Product peaches = new Product("Персики", 1, 420f);
+        Product cherries = new Product("Вишня", 3, 320f);
+        Product apricots = new Product("Абрикосы", 1.5f, 230f);
+        Product chicken = new Product("Курица", 2.1f, 283f);
+        Product pasta = new Product("Макароны", 0.9f, 27f);
+        Product buckwheat = new Product("Гречка", 1.8f, 148f);
+        Product onion = new Product("Лук", 0.5f, 24f);
+        Product blackPepper = new Product("Черный перец", 0.05f, 15f);
+        Product redPepper = new Product("Красный перец", 0.05f, 16f);
+
+        System.out.println(basket);
+
+        apples.addProducts(basket);
+        peaches.addProducts(basket);
+        cherries.addProducts(basket);
+        apricots.addProducts(basket);
+        chicken.addProducts(basket);
+        pasta.addProducts(basket);
+        buckwheat.addProducts(basket);
+        onion.addProducts(basket);
+        blackPepper.addProducts(basket);
+        redPepper.addProducts(basket);
+        System.out.println(basket);
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,93 @@
+import java.util.Objects;
+import java.util.Set;
+
+public class Product {
+
+    private String name;
+    private float amount;
+    private float cost;
+    private boolean bought;
+
+    public Product(String name, float amount, float cost) {
+        this.name = validateParameters(name);
+        this.amount = validateNumbers(amount);
+        this.cost = validateNumbers(cost);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public boolean isBought() {
+        return bought;
+    }
+
+    public void setBought(boolean bought) {
+        this.bought = bought;
+    }
+
+    public void addProducts(Set<Product> basket) {
+        if (!basket.contains(this)) {
+            basket.add(this);
+            this.bought = true;
+        }else {
+            throw new UnsupportedOperationException("Такой товар " + getName() + " уже есть в корзине");
+        }
+    }
+
+    public String validateParameters(String value) {
+        if (value == null || value.isEmpty() || value.isBlank()) {
+            throw new UnsupportedOperationException("Не ввёл название продукта");
+        } else {
+            return value;
+        }
+    }
+
+    public float validateNumbers(float value) {
+        if (value == 0) {
+            throw new UnsupportedOperationException("Введите количество");
+        } else if (value < 0) {
+            return value = Math.abs(value);
+        } else {
+            return value;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + ", количество " + amount + " кг, стоимость " + cost + " рублей" +'\n';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Float.compare(product.amount, amount) == 0 && Float.compare(product.cost, cost) == 0 && bought == product.bought && name.equals(product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, amount, cost, bought);
+    }
+}
