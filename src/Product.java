3a0ec0faf991beb4ac747488d7ff3d131d34350d
YuryShageev <schageev.yur@yandex.ru>
2022-12-14 23:56:31 +0300

    Продукты

--- src/Product.java
+++ src/Product.java
@@ -13,6 +13,10 @@ public class Product {
         this.cost = validateNumbers(cost);
     }
 
+    public Product(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
